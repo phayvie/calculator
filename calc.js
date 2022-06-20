f646bbcbbffbfc05f30408299922e9533dfa1288
Favour Onyeaghala <onyeaghalafavourr@gmail.com>
2022-06-20 20:56:51 +0100

    first commit

---
+++ calc.js
@@ -0,0 +1,25 @@
+let result= document.getElementById("inputtext")
+ 
+
+ let calculate=(number)=>{
+     result.value+=number;
+ }
+
+ let Result=()=>{
+     try{
+         result.value=eval(result.value)
+     }
+
+     catch(err){
+     result.value=("INAVLID INPUT")
+     }
+ }
+
+
+ function clr(){
+     result.value=" ";
+ }
+
+ function del(){
+     result.value=result.value.slice(0,-1)
+ }
